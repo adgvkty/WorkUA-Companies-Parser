64985eb15ec71e05d6e4b47ef0d93b1966d08153
adgvkty <48160200+adgvkty@users.noreply.github.com>
2021-10-26 18:06:33 +0300

    Main -

---
+++ go.mod
@@ -0,0 +1,40 @@
+module shop_parsing_bot
+
+go 1.17
+
+require (
+	github.com/bingoohuang/xlsx v1.1.0
+	github.com/gocolly/colly v1.2.0
+	github.com/unidoc/unioffice v1.3.0
+)
+
+require (
+	github.com/PuerkitoBio/goquery v1.7.1 // indirect
+	github.com/andybalholm/cascadia v1.2.0 // indirect
+	github.com/antchfx/htmlquery v1.2.4 // indirect
+	github.com/antchfx/xmlquery v1.3.7 // indirect
+	github.com/antchfx/xpath v1.2.0 // indirect
+	github.com/araddon/dateparse v0.0.0-20190622164848-0fb0a474d195 // indirect
+	github.com/averagesecurityguy/random v0.0.0-20180326012618-37cce341d2af // indirect
+	github.com/bingoohuang/gor v0.0.0-20200321095356-94cf6290c6ea // indirect
+	github.com/bingoohuang/strcase v0.0.0-20200312105414-ac2c85cfc85d // indirect
+	github.com/gobwas/glob v0.2.3 // indirect
+	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
+	github.com/golang/protobuf v1.3.2 // indirect
+	github.com/kennygrant/sanitize v1.2.4 // indirect
+	github.com/konsorten/go-windows-terminal-sequences v1.0.1 // indirect
+	github.com/mohae/deepcopy v0.0.0-20170929034955-c48cc78d4826 // indirect
+	github.com/richardlehane/mscfb v1.0.3 // indirect
+	github.com/richardlehane/msoleps v1.0.1 // indirect
+	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
+	github.com/sirupsen/logrus v1.5.0 // indirect
+	github.com/temoto/robotstxt v1.1.2 // indirect
+	github.com/thoas/go-funk v0.5.0 // indirect
+	github.com/xuri/efp v0.0.0-20210322160811-ab561f5b45e3 // indirect
+	github.com/xuri/excelize/v2 v2.4.1 // indirect
+	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
+	golang.org/x/net v0.0.0-20210929193557-e81a3d93ecf6 // indirect
+	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
+	golang.org/x/text v0.3.6 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+)
---
+++ main.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/gocolly/colly"
+	"github.com/xuri/excelize/v2"
+)
+
+const (
+	listLink    string = "https://www.work.ua/ru/jobs/by-company/by-industry/it/?page="
+	companyLink string = "https://www.work.ua"
+	xmlString   string = "/html/body/section/div/div[3]"
+)
+
+// Company ...
+type Company struct {
+	name        string
+	website     string
+	workUA      string
+	description string
+	placesCount int
+}
+
+// NewCompany ...
+func NewCompany(name, website, workUA, descr string) Company {
+
+	log.Printf("creating new company: %v\n", name)
+	return Company{
+		name:        name,
+		website:     website,
+		workUA:      workUA,
+		description: descr,
+	}
+}
+
+func (c Company) incrementPlaces() Company {
+	c.placesCount++
+	return c
+}
+
+func main() {
+	var companies map[string]Company = map[string]Company{}
+
+	for page := 1; page <= 127; page++ {
+		log.Printf("new loop: page %v\n", page)
+		pageCollector := colly.NewCollector()
+		companyCollector := colly.NewCollector()
+		cityCollector := colly.NewCollector()
+
+		companyCollector.OnXML("/html/body/section/div/div/div[1]/div[2]", func(e *colly.XMLElement) {
+			companies[e.Request.URL.String()] = NewCompany(
+				e.ChildText("div/h1"),
+				e.ChildAttr("div/div/div/p/span/a", "href"),
+				e.Request.URL.String(),
+				fmt.Sprintf("%v %v", e.ChildText("div/p[1]"), e.ChildText("div/p[2]")))
+		})
+
+		pageCollector.OnXML(fmt.Sprintf(xmlString), func(e *colly.XMLElement) {
+			for companyCard := 2; companyCard <= 23; companyCard++ {
+				link := companyLink + e.ChildAttr(fmt.Sprintf("div[%v]/div/div[2]/h2/a", companyCard), "href")
+				companyCollector.Visit(link)
+				cityCollector.Visit(link)
+			}
+		})
+
+		cityCollector.OnHTML("div.card.card-hover.card-visited.wordwrap.job-link", func(e *colly.HTMLElement) {
+			if strings.Contains(e.ChildText("span"), "Киев") {
+				companies[e.Request.URL.String()] = companies[e.Request.URL.String()].incrementPlaces()
+			}
+		})
+
+		pageCollector.OnRequest(func(r *colly.Request) {
+			time.Sleep(5 * time.Second)
+			log.Println("Visiting", r.URL)
+		})
+
+		pageCollector.OnError(func(r *colly.Response, e error) {
+			log.Println("error:", e)
+		})
+
+		pageCollector.Visit(listLink + strconv.Itoa(page))
+
+	}
+
+	f := excelize.NewFile()
+	index := f.NewSheet("Sheet1")
+	f.SetCellValue("Sheet1", "A1", "Company Name")
+	f.SetCellValue("Sheet1", "B1", "Work.ua")
+	f.SetCellValue("Sheet1", "C1", "Website")
+	f.SetCellValue("Sheet1", "D1", "Company Description")
+	f.SetCellValue("Sheet1", "E1", "Places")
+	counter := 2
+
+	for _, company := range companies {
+		if company.placesCount != 0 && company.name != "" && !strings.Contains(company.name, "ФОП") && company.website != "" {
+			f.SetCellValue("Sheet1", fmt.Sprintf("A%v", counter), company.name)
+			f.SetCellValue("Sheet1", fmt.Sprintf("B%v", counter), company.workUA)
+			f.SetCellValue("Sheet1", fmt.Sprintf("C%v", counter), company.website)
+			f.SetCellValue("Sheet1", fmt.Sprintf("D%v", counter), company.description)
+			f.SetCellValue("Sheet1", fmt.Sprintf("E%v", counter), company.placesCount)
+			counter++
+		}
+	}
+
+	f.SetActiveSheet(index)
+
+	if err := f.SaveAs("Book1.xlsx"); err != nil {
+		log.Println(err)
+	}
+
+}
